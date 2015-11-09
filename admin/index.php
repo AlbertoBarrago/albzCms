06e9f8e99a15e69cb66106d917459202c7d8385f
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-11-09 21:34:39 +0100

    Adding admin-

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,10 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
 
     <div id="wrapper">
 
-      
+
       <!-- Nagivation -->
-      <?php include "includes/navigation.php"; ?>
+      <?php include "includes/admin_navigation.php"; ?>
 
         <div id="page-wrapper">
 
@@ -35,4 +35,4 @@
         </div>
         <!-- /#page-wrapper -->
 
-    <?php include "includes/footer.php"; ?>
+    <?php include "includes/admin_footer.php"; ?>
