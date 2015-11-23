7434b039e0d399da7137559c103c43c49d385398
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-11-23 20:42:56 +0100

    fixing BUG
    that crate a false count of comments +1 when a reset views on backend

--- post.php
+++ post.php
@@ -16,11 +16,10 @@
 
                 if(isset($_GET['p_id'])) {
 
-                  $the_post_id = $_GET['p_id'];
+                $the_post_id = $_GET['p_id'];
 
-
-                $query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
-                $send_query = mysqli_query($connection,$query);
+                $view_query = $query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
+                $send_query = mysqli_query($connection,$view_query);
 
                 $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
                 $select_all_posts_query = mysqli_query($connection,$query);
@@ -81,6 +80,10 @@
 
                     }
 
+                  $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                  $query .= "WHERE post_id = $the_post_id ";
+                  $update_comment_count = mysqli_query($connection, $query);
+
                 }
 
               ?>
@@ -119,10 +122,6 @@
               if(!$select_comment_query) {
                 die('Query Failed' . mysqli_error($connection));
               }
-
-              $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-              $query .= "WHERE post_id = $the_post_id ";
-              $update_comment_count = mysqli_query($connection, $query);
 
 
               while ($row = mysqli_fetch_array($select_comment_query)) {
