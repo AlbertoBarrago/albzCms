b689cc8d1e09062a290a33592a06a5a42fe70306
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-11-27 22:16:13 +0100

    Update post_user instead post_author

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                 while($row = mysqli_fetch_array($select_all_posts_query)) {
                   $post_id = $row['post_id'];
                   $post_title = $row['post_title'];
-                  $post_author = $row['post_author'];
+                  $post_user = $row['post_user'];
                   $post_date = $row['post_date'];
                   $post_image = $row['post_image'];
                   $post_content = substr($row['post_content'],0,100);
@@ -73,7 +73,7 @@
                     <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                   </h2>
                   <p class="lead">
-                      by <a href="author_post.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+                      by <a href="author_post.php?author=<?php echo $post_user; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_user; ?></a>
                   </p>
                   <p><span class="glyphicon glyphicon-time"></span> Posted on August <?php echo $post_date; ?></p>
                   <hr>
