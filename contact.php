fbb6b5f7c94a49a152b1250325f0025cf8fa47fc
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-11-29 13:49:35 +0100

    Fixed error
    :P

--- contact.php
+++ contact.php
@@ -9,21 +9,34 @@
 
 <?php
 
+  if(isset($_POST['sendEmail'])){
 
-  if(isset($_POST['submit'])){
-
-      $to         = 'studio@albertobarrago.it';
-      $subject    = $_POST['subject'];
-      $body       = $_POST['body'];
-
-      $body  = $msg;
       $msg   = wordwrap($msg,70);
 
-      mail($to,$subject,$msg);
+      $to         = 'studio@albertobarrago.it';
+      $subject    = wordwrap($_POST['subject'],70);
+      $body       = $_POST['body'];
 
-      echo '<div class="alert alert-success" role="alert"> Messaggio inviato corretamente a ' . $to . '</div> ';
 
-  }
+      if(!empty($subject) && !empty($body)){
+
+        mail($to,$subject,$body);
+
+        echo '<div class="alert alert-success" role="alert"> Messaggio inviato corretamente a ' . $to . '</div> ';
+
+
+
+
+      }else {
+
+
+        echo '<div class="alert alert-danger" role="alert"> Compila i campi richiesti '. session_id() .' </div> ';
+
+
+      }
+
+
+    }
 
 
 ?>
@@ -37,21 +50,20 @@
                 <div class="form-wrap">
                 <h1>Contact</h1>
 
-                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                    
+                    <form role="form" action="contact.php" method="post" id="login-form" autocomplete="off">
                         <div class="form-group">
-                            <label for="email" class="sr-only">Email</label>
+                            <label for="email" class="sr-only">Email*</label>
                             <input type="email" name="email" id="email" class="form-control" placeholder="someusername@example.com">
                         </div>
                         <div class="form-group">
-                            <label for="subject" class="sr-only">subject</label>
+                            <label for="subject" class="sr-only">Subject*</label>
                             <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your subject">
                         </div>
                          <div class="form-group">
                             <textarea name="body" rows="8" cols="40" class="form-control"></textarea>
                         </div>
 
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Invia Email">
+                        <input type="submit" name="sendEmail" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Invia Email">
                     </form>
 
                 </div>
