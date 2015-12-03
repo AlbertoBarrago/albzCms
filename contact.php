ebb8d5626f5d1584c8a8628bc0518a2ca0900007
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-12-03 18:28:38 +0100

    Update headers

--- contact.php
+++ contact.php
@@ -27,7 +27,7 @@
       $body       = $_POST['body'];
 	 $headers  = 'MIME-Version: 1.0' . "\r\n";
 	 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	 $headers .= 'From: alBz Blog ' . $validate . "\r\n";
+	 $headers .= 'From: ' . $validate . "\r\n";
  
 
 
