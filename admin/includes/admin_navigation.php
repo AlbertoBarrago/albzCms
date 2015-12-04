cb1d0c7732fb413cc63609eafd3bd35c8ca6ae66
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-12-04 22:19:20 +0100

    link comments page

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -63,7 +63,7 @@
                     <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
                 </li>
                 <li>
-                    <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+                    <a href="comments.php"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
                 </li>
                 <li>
                     <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
