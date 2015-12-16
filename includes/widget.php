6a0cb8711962cffb75f85b2577dad2e44d60fd63
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-12-16 22:01:18 +0100

    Added Disclaimer

--- includes/widget.php
+++ includes/widget.php
@@ -1,4 +1,7 @@
 <div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    <h4>Disclaimer</h4>
+    <p>Benvenuto all'interno di <b>alBzCms</b> Sei libero di chiedermi o inviarmi consigli e miglioramenti. Grazie!</p>
+    <p>Welcome in  <b>alBzCms</b> You are free to ask for advice and send improvements. Thank you!</p>	
+     <a href="mailTo:studio@albertobarrago.it?subject=cmsEmail">Inviami <i class="fa fa-envelope-o"></i></a> <br/>
+    <a href="https://github.com/AlbertoBarrago/albzCms/archive/master.zip">Scarica Repo da <i class="fa fa-github"></i></a>
 </div>
